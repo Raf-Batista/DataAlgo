b2ec0e160a601d2cea512bacd64bb31087b97787
Rafael Batista <RafaelB2424@gmail.com>
2020-10-10 16:32:02 -0400

    Initial commit

---
+++ JavaScript/Anagrams In A String/anagramsInString.js
@@ -0,0 +1,11 @@
+// not solved
+
+function anagramsInString(s, t) {
+    const result = []; 
+
+    // .split('').sort().join('')
+    // .split('').splice(0,2).sort().join('')
+    return result;
+}
+
+module.exports = anagramsInString
